d0d54c173731f099d552282e7ae48e5040dec5a7
ustinski <qwertyuiop231571@gmail.com>
2016-02-27 20:47:48 +0200

    app sample

---
+++ source/src/app.cpp
@@ -0,0 +1,25 @@
+#include "app.h"
+
+#include "../sgm/app.h"
+
+#include <GLES2/gl2.h>
+
+namespace App
+{
+    
+}
+
+void App::init(int width, int height)
+{
+    
+}
+
+void App::tick(unsigned int dt)
+{
+    
+}
+
+void App::close()
+{
+    
+}
---
+++ source/src/app.h
@@ -0,0 +1,9 @@
+#ifndef APP_H
+#define APP_H
+
+namespace App
+{
+
+}
+
+#endif // APP_H
