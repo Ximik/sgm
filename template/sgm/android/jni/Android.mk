bcb5b4728460debc6d5770dd863e208f34c8378c
Alex Tsokurov <me@ximik.net>
2016-03-11 02:49:28 +0200

    build fixes

--- template/sgm/android/jni/Android.mk
+++ template/sgm/android/jni/Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH := $(call my-dir)
+SRC_DIR := ../../../src
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := app
 LOCAL_CFLAGS    := -Wall -Wextra
-LOCAL_SRC_FILES := jni.c hook.cpp ../../app.cpp
+LOCAL_SRC_FILES :=
 LOCAL_LDLIBS := -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
